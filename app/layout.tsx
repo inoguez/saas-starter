c6708af91edc47d3b1d0f35e8b17a731ea922ebf
inoguez <sk8ian100@gmail.com>
2025-02-09 03:17:11 -0600

    fix: dashboard layout cleaned

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import type { Metadata, Viewport } from 'next';
 import { Geist } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
-import { cn } from '@/lib/utils';
-import { Header } from '@/components/landing/landing-header';
-import Footer from '@/components/landing/landing-footer';
 
 export const metadata: Metadata = {
   title: 'Realita MX',
@@ -24,11 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   let userPromise = getUser();
-  /* <section className={cn('mx-auto flex flex-col min-h-screen p-6 ')}>
-    <Header />
-    <div className='flex-1'>{children}</div>
-    <Footer />
-  </section> */
+
   return (
     <html lang='es' className={`${geist.className}`}>
       <body className='min-h-[100dvh] bg-gradient-to-b from-jet-black to-jade-gradient bg-jet-black '>
